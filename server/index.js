46b4d46bd27d4d9ee4039477c41d49765097dc52
manuelMarkDenver <manuel.markdenver@gmail.com>
2022-10-21 01:11:15 +0800

    heroku

--- server/index.js
+++ server/index.js
@@ -22,3 +22,13 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
 })
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
